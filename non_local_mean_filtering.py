411324d610c4ebf63fd20731a25de062ab428047
sanid3301 <47135651+sanid3301@users.noreply.github.com>
2021-10-05 14:56:17 +0530

    Add files via upload

---
+++ non_local_mean_filtering.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  5 14:09:28 2021
+
+@author: Sanidhya
+"""
+
+import cv2
+import numpy as np
+from skimage import io, img_as_float
+from skimage.restoration import denoise_nl_means, estimate_sigma
+
+img = img_as_float(io.imread('D:/Academics/IITD/7th Sem/BTP/Image data/5.jpg'))
+
+sigma_est = np.mean(estimate_sigma(img, multichannel = False))
+
+denoising_img = denoise_nl_means(img, h=1.25*sigma_est, fast_mode=True, 
+                                 patch_size=5, 
+                                 patch_distance=3, 
+                                 multichannel= False)
+
+cv2.imshow('Original', img)
+cv2.imshow('Denoised', denoising_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+         
